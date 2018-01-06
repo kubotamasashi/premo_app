66d90ee7d22ed8ac70d178a50215080f028bcebb
kubota masashi <jyvina@gmail.com>
2018-01-06 05:30:19 +0000

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+PremoApp::Application.config.secret_key_base = '9789eb2c8e340ddd7ee2e57b3a3262b8b2e84149ba9aa6c457da82a914f30c63aea473a4dcc3f33840e95b1ce50a7591a23de39b644b8b29ef0a09d7bb8628b0'
